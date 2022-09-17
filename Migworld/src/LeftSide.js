f54f1a1207137b492178a35924f809f998a4f43f
tomkaX <tomacolyada@gmail.com>
2022-09-17 10:36:51 +0200

    update width

--- Migworld/src/LeftSide.js
+++ Migworld/src/LeftSide.js
@@ -209,7 +209,7 @@ define(["exports"], function (exports) {
               return React.createElement(
                 "div",
                 null,
-                React.createElement("img", { src: "images/radarex.png", width: "50%", style: { maxWidth: "100px", marginTop: "20px", marginBottom: "20px" } }),
+                React.createElement("img", { src: "images/radarex.png", width: "100%", style: { maxWidth: "100px", marginTop: "20px", marginBottom: "20px" } }),
                 React.createElement(
                   "p",
                   null,
